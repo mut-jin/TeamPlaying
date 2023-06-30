34711ae39ebef8a8baafa6dde177d1a77ec3aa6b
jm <dkssud2422@naver.com>
2023-06-30 17:16:42 +0900

    first commit

---
+++ src/test/java/com/example/teamplaying/TeamPlaying2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.teamplaying;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamPlaying2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
