287f380c5ae98dc771a970ff716b3387fea237b2
EveryoneStandUp <wjsgudwns98@naver.com>
2023-08-01 09:15:29 +0900

    0801작업

--- src/main/java/com/example/teamplaying/service/CsService.java
+++ src/main/java/com/example/teamplaying/service/CsService.java
@@ -159,6 +159,7 @@ public class CsService {
 
     public String getWriterById(Integer id) {
         return csMapper.getWriter(id);
+    }
 
     public List<CsBoard> getAllCs() {
         return csMapper.selectAllCs();
